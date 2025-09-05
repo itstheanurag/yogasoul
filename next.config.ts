782c8bb9c7bd194dbdfa85f03ae644295d41033f
gaurav <gauravanurag36@gmail.com>
2025-09-05 11:09:47 +0530

    fix: for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+  output: "export",
+  images: {
+    unoptimized: true, // needed since GitHub Pages can’t handle Next.js Image Optimization
+  },
+  trailingSlash: true, // recommended for GitHub Pages to avoid 404s
+};
+
+export default nextConfig;
